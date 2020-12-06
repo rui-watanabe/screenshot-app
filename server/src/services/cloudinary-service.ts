479712cc9e71cb93d97598157cd7290e9993f743
rui-watanabe <wataru610806@gmail.com>
2020-12-06 20:00:43 +0900

    setup nats service #3

--- server/src/services/cloudinary-service.ts
+++ server/src/services/cloudinary-service.ts
@@ -12,15 +12,15 @@ class CloudinaryService {
 
   connect(){
     if(!process.env.CLOUDINARY_CLIENT_NAME){
-      throw new Error("Not defined process.env.CLOUDINARY_CLIENT_NAME")
+      throw new Error("Not defined process.env.CLOUDINARY_CLIENT_NAME");
     }
 
     if(!process.env.API_PUBLIC_KEY){
-      throw new Error("Not defined process.env.process.env.API_PUBLIC_KEY")
+      throw new Error("Not defined process.env.process.env.API_PUBLIC_KEY");
     }
 
     if(!process.env.API_SECRET_KEY){
-      throw new Error("Not defined process.env.process.env.API_SECRET_KEY")
+      throw new Error("Not defined process.env.process.env.API_SECRET_KEY");
     }
 
     this._cloudinary = cloudinary.v2;
@@ -29,7 +29,7 @@ class CloudinaryService {
       api_public_key: process.env.API_PUBLIC_KEY,
       api_secret_key: process.env.API_SECRET_KEY
     });
-  }
+  };
 }
 
 export const cloudinaryService = new CloudinaryService();
