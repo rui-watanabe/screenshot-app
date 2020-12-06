656f3711ff2c74745506348d473d35e7d195c1a3
rui-watanabe <wataru610806@gmail.com>
2020-12-06 18:05:06 +0900

    set base listner #1

---
+++ common/src/events/base-listener.ts
@@ -0,0 +1,33 @@
+import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Lister<T extends Event> {
+  protected client: Stan;
+  abstract subject: T['subject'];
+  abstract onMessage(data: T['data'], msg: Message);
+
+  constructor(client: Stan){
+    this.client = client;
+  };
+
+  listener() {
+    const subscription = this.client.subscribe(this.subject);
+    subscription.on('message', (msg: Message) => {
+      console.log(`Received Event ${msg.getSequence()} ${msg.getSubject()}`);
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
