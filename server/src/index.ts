e049eaa834511be12b2340b0a59ed953b7a7377d
rui-watanabe <wataru610806@gmail.com>
2020-12-12 13:28:21 +0900

    add base structure of index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
-console.log("server start");
+import { ScreenshotCreatedEventListener } from "./events/listeners/screenshot-listener-created";
+import { cloudinaryService } from "./services/cloudinary-service";
+import { natsService } from "./services/nats-service";
+
+const start = async () => {
+  if(!process.env.NATS_CLUSTER_ID){
+    throw new Error("Not defined process.env.NATS_CLUSTER_ID");
+  }
+
+  if(!process.env.NATS_CLIENT_ID){
+    throw new Error("Not defined process.env.NATS_CLIENT_ID");
+  }
+
+  if(!process.env.NATS_URL){
+    throw new Error("Not defined process.env.NATS_URL");
+  }
+
+  try {
+    await natsService.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
+    natsService.client.on('close', () => {
+      console.log('NATS connection close');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => { natsService.client.close(); });
+    process.on('SIGTERM', () => { natsService.client.close(); });
+
+    cloudinaryService.connect();
+
+    new ScreenshotCreatedEventListener(natsService.client).listen();
+  }
+  catch(err) {
+    console.log(err);
+  }
+};
+
+start();
