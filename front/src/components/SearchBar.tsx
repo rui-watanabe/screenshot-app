2d9008b34811fae5193400cbb9498c15a6b3d14b
rui-watanabe <wataru610806@gmail.com>
2020-12-19 19:40:36 +0900

    add Searchbar component #6

---
+++ front/src/components/SearchBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const SearchBar = ({ auth, url }, onSubmit ) => {
+
+  const [value, setValue] = useState("");
+
+  function onFormSubmit(e) {
+    e.prevent.default();
+    onSubmit(url);
+  }
+
+  return( 
+    <div className="ui segment">
+      <div className="item">
+        <form className="ui form" onSubmit={ onFormSubmit }>
+          <div className={`ui labeled field icon input ${auth ? '' : 'disabled'}`}>
+            <div className="ui label">http://</div>
+            <input 
+              value={ url }
+              onSubmit={e => setValue({ url: e.target.value })}
+              type="text" 
+              placeholder="Take Screenshot"
+            />
+          </div>
+          <i className="search link icon"></i>
+        </form>
+      </div>
+    </div>
+  )
+};
+
+export default SearchBar;
