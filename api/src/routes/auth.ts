67aafd43850ff9b1d832cbb9d104672154356c22
rui-watanabe <wataru610806@gmail.com>
2020-12-19 16:45:12 +0900

    add screenshots routes of api #5

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { passport } from '../services/passport-service';
 
 const router = express.Router();
@@ -7,16 +7,16 @@ router.get('api/login', passport.authenticate('google', {
   scope: ['profile', 'email']
 }));
 
-router.get('api/login/callback', passport.authenticate('google', (req, res) => {
+router.get('api/login/callback', passport.authenticate('google', (req: Request, res: Response) => {
   res.redirect('/');
 }));
 
-router.get('api/login/logout', passport.authenticate('google', (req, res) => {
+router.get('api/login/logout', passport.authenticate('google', (req: Request, res: Response) => {
   req.logout();
   res.redirect('/');
 }));
 
-router.get('api/me', (req, res) => {
+router.get('api/me', (req: Request, res: Response) => {
   res.send(req.user);
 });
 
